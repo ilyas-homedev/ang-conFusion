8576440e833ee0400962bd4ca7301d097a083880
ilyas-homedev <ilyasov000fp@gmail.com>
2021-05-17 19:16:04 +0200

    Single Page Applications Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -11,5 +11,6 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
-    { path: 'menu', component: MenuComponent }
+    { path: 'menu', component: MenuComponent },
+    { path: 'dishdetail/:id', component: DishdetailComponent }
 ]
