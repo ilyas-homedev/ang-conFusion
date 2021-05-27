a60d27c61668d305e9e90db36d451ed8b8712751
ilyas-homedev <ilyasov000fp@gmail.com>
2021-05-27 15:08:15 +0200

    Promise Part 1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-menu',
@@ -15,7 +16,8 @@ export class MenuComponent implements OnInit {
   constructor(private dishService: DishService) { }
 
   ngOnInit(): void {
-    this.dishes = this.dishService.getDishes();
+    this.dishService.getDishes()
+    .then((dishes) => this.dishes = dishes);
   }
 
   onSelect(dish: Dish) {
